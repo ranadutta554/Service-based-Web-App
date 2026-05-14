2df227f54cd1b31da0872ea325b5ee263a51b284
Rana Dutta <your-email@gmail.com>
2026-05-14 22:47:25 +0530

    Initial clean commit

--- Tution/com.education.info/src/main/java/com/edu/info/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edu.info.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edu.info.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
--- Tution/com.education.info/src/main/java/com/edu/info/repository/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edu.info.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edu.info.entity.RegistrationEntity;
-
-public interface RegistrationRepository extends JpaRepository<RegistrationEntity, String>{
-
-}
